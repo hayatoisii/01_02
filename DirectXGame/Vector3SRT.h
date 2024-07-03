b8bf482a418a5f13e4c84a8bf19597b89ff52fd5
hayatoisii <k023g0022@g.neec.ac.jp>
2024-07-03 12:11:21 +0900

    完了

--- DirectXGame/Vector3SRT.h
+++ DirectXGame/Vector3SRT.h
@@ -7,19 +7,24 @@
 #include <stdio.h>
 #include <4x4Matrixcalculation.h>
 
-//回転
-Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
-//拡大
-Matrix4x4 MakeScaleMatrix(const Vector3& scale);
-//同時座標変換
-Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
-
-//回転X
 Matrix4x4 MakeRotateXMatrix(float radian);
-//回転Y
+
 Matrix4x4 MakeRotateYMatrix(float radian);
-//回転Z
+
 Matrix4x4 MakeRotateZMatrix(float radian);
 
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
 
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix);
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
 Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+// viewport
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
+// 透視投影行列
+Matrix4x4 MakePerspectiveMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
