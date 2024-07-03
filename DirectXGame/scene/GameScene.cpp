1a4ccc3bf26d5b0975aa9f6539dc8cfd1262100c
hayatoisii <k023g0022@g.neec.ac.jp>
2024-07-03 12:22:16 +0900

    dekisou

--- DirectXGame/scene/GameScene.cpp
+++ DirectXGame/scene/GameScene.cpp
@@ -135,9 +135,8 @@ void GameScene::Update() {
 
 	if (isDebugCameraActive_) {
 
-		debugCamera_->Update();
-		viewProjection_.matView = debugCamera_->GetView();
-		viewProjection_.matProjection = debugCamera_->GetProjection();
+		viewProjection_.matView = debugCamera_->GetViewProjection().matView;
+		viewProjection_.matProjection = debugCamera_->GetViewProjection().matProjection;
 
 		//
 		viewProjection_.TransferMatrix();
