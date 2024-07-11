77f7d0ccee4968d22825cfe20ffea7d4940c733c
hayatoisii <k023g0022@g.neec.ac.jp>
2024-07-11 16:02:46 +0900

    完成

--- DirectXGame/Player.cpp
+++ DirectXGame/Player.cpp
@@ -6,7 +6,9 @@
 
 #include <ImGuiManager.h>
 
-float lerp(float start, float end, float t);
+float EaseInOut(float x1, float x2, float t);
+
+float Lerp(float start, float end, float t);
 
 Player::~Player() {
 	model_ = nullptr;
@@ -34,21 +36,23 @@ void Player::Initialize(Model* model, ViewProjection* viewProjection, const Vect
 // 更新
 void Player::Update() {
 
-	// 移動入力
+// 移動入力
 	if (onGround_) {
+		// 左右移動操作
+		if (Input::GetInstance()->PushKey(DIK_RIGHT) || Input::GetInstance()->PushKey(DIK_LEFT)) {
 
-		if (Input::GetInstance()->PushKey(DIK_A) || Input::GetInstance()->PushKey(DIK_D)) {
-			worldTransform_.rotation_.y = std::numbers::pi_v<float> / 2.0f;
 			// 左右加速
 			Vector3 acceleration = {};
 
-			if (Input::GetInstance()->PushKey(DIK_D)) {
-				worldTransform_.rotation_.y = std::numbers::pi_v<float> * 3.0f / 2.0f;
-				acceleration.x += kAcceleration;
-
+			if (Input::GetInstance()->PushKey(DIK_RIGHT)) {
+				// 左移動中の右入力
 				if (velocity_.x < 0.0f) {
+					// 速度と逆方向に入力中は急ブレーキ
 					velocity_.x *= (1.0f - kAttenuation);
 				}
+
+				acceleration.x += kAcceleration;
+
 
 				if (lrdDirection_ != LRDirection::kRight) {
 					lrdDirection_ = LRDirection::kRight;
@@ -57,19 +61,20 @@ void Player::Update() {
 					// 旋回タイマーに時間を設定する
 					turnTimer_ = kTimeTurn;
 				}
-			} else if (Input::GetInstance()->PushKey(DIK_A)) {
-
-				acceleration.x -= kAcceleration;
-
+			} else if (Input::GetInstance()->PushKey(DIK_LEFT)) {
+				// 右移動中の左入力
 				if (velocity_.x > 0.0f) {
+					// 速度と逆方向に入力中は急ブレーキ
 					velocity_.x *= (1.0f - kAttenuation);
 				}
 
+				acceleration.x -= kAcceleration;
+
 				if (lrdDirection_ != LRDirection::kLeft) {
 					lrdDirection_ = LRDirection::kLeft;
-					// 旋回開始時の角度を記録する
+					// 旋回開始時の角度
 					turnFirstRotationY_ = worldTransform_.rotation_.y;
-					// 旋回タイマーに時間を設定する
+					// 旋回タイマー
 					turnTimer_ = kTimeTurn;
 				}
 			}
@@ -77,6 +82,8 @@ void Player::Update() {
 			velocity_.x += acceleration.x;
 			velocity_.y += acceleration.y;
 			velocity_.z += acceleration.z;
+
+
 
 			// 最大速度制限
 			velocity_.x = std::clamp(velocity_.x, -kLimitRunSpeed, kLimitRunSpeed);
@@ -104,12 +111,20 @@ void Player::Update() {
 		velocity_.y = std::max(velocity_.y, -kLimitFallSpeed);
 	}
 
-	// 左右の自キャラ角度テーブル
-	float destinationRotationYTable[] = {std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f};
-	// 状態に応じた角度を取得する
-	float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrdDirection_)];
-	// 自キャラの角度を設定する
-	worldTransform_.rotation_.y = destinationRotationY;
+	if (turnTimer_ > 0.0f) {
+
+		turnTimer_ -= static_cast<float>(1.0f) / 60.0f;
+
+		// 左右の自キャラ角度テーブル
+		float destinationRotationYTable[] = {
+		    std::numbers::pi_v<float> / 2.0f, std::numbers::pi_v<float> * 3.0f / 2.0f
+
+		};
+		// 状態に応じた目標角度を取得する
+		float destinationRotationY = destinationRotationYTable[static_cast<uint32_t>(lrdDirection_)];
+		// 自キャラ角度を設定する
+		worldTransform_.rotation_.y = EaseInOut(destinationRotationY, turnFirstRotationY_, turnTimer_ / kTimeTurn);
+	}
 
 	// 着地フラグ
 	bool landing = false;
@@ -145,6 +160,8 @@ void Player::Update() {
 	}
 
 
+	
+
 	// 移動
 	worldTransform_.translation_.x += velocity_.x;
 	worldTransform_.translation_.y += velocity_.y;
@@ -152,9 +169,6 @@ void Player::Update() {
 
 	// 行列計算
 	worldTransform_.UpdateMatarix();
-	// 行列を定数バッファに転送
-	worldTransform_.TransferMatrix();
-
 
 }
 // 描画
@@ -162,6 +176,12 @@ void Player::Draw() {
 	// 3D作成
 	model_->Draw(worldTransform_, *viewProjection_);
 }
-float lerp(float start, float end, float t) {
+
+float Lerp(float start, float end, float t) {
 	return start + t * (end - start); // 修正された補間公式
+}
+
+float EaseInOut(float x1, float x2, float t) { 
+	float easedT = -(std::cosf(std::numbers::pi_v<float> * t) - 1.0f) / 2.0f; 
+	return Lerp(x1, x2, easedT);
 }
