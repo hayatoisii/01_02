a4b34d180faa2da0d69076fc56c6b866c2725b81
hayatoisii <k023g0022@g.neec.ac.jp>
2024-08-05 14:38:06 +0900

    aa

---
+++ DirectXGame/Enemy.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "Model.h"
+#include "ViewProjection.h"
+#include "WorldTransform.h"
+
+class Enemy {
+
+public:
+	void Initialize(Model* model, ViewProjection* viewProjection, const Vector3& position);
+
+	void Update();
+
+	void Draw();
+
+private:
+
+	Model* model_ = nullptr;
+	WorldTransform worldTranform_;
+	ViewProjection* viewProjection_ = nullptr;
+
+};
