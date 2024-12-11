90096a7b505753b77ec1ce2994ce48d99048408e
hayatoisii <k023g0022@g.neec.ac.jp>
2024-12-11 17:02:27 +0900

    aaa

--- DirectXGame/MapChipField.h
+++ DirectXGame/MapChipField.h
@@ -27,8 +27,8 @@ public:
 	private:
 
 			// 1ブロックのサイズ
-	    static inline const float kBlockWidth = 2.0f;
-	    static inline const float kBlockHeight = 2.0f;
+	    static inline const float kBlockWidth = 8.0f;
+	    static inline const float kBlockHeight = 6.0f;
 	    // ブロックの個数
 	    static inline const uint32_t kNumBlockVirtical = 20;
 	    static inline const uint32_t kNumBlockHorizontal = 20;
